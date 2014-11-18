616fb64d1ea4241c5465b5bc7b6848cfa5b7f497
Kim Desrosiers <kimdesro@gmail.com>
2014-11-17 21:11:20 -0500

    setting modif

--- gobgift/settings.py
+++ gobgift/settings.py
@@ -102,7 +102,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/home/kdesrosiers/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
