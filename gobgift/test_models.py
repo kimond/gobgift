eb81da72b93483f0384c85956b202884730a4062
Kim Desrosiers <kimdesro@gmail.com>
2016-08-01 11:07:02 -0400

    add models test

--- gobgift/test_models.py
+++ gobgift/test_models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from gobgift.models import ListGroup, Liste, Gift
+from gobgift.models import ListGroup, Liste, Gift, ListGroupUser
 
 
 class ListGroupTest(TestCase):
@@ -19,3 +20,11 @@ class GiftTest(TestCase):
     def test_string_representation(self):
         gift = Gift(name="Super gift")
         self.assertEquals(str(gift), gift.name)
+
+
+class ListGroupUserTest(TestCase):
+    def test_string_representation(self):
+        user = User.objects.create_user(username="johnd", first_name="John", email="test@test.com", last_name="Deer")
+        list_group_user = ListGroupUser(user=user)
+        string_representation = user.first_name + " " + user.last_name
+        self.assertEquals(str(list_group_user), string_representation)
