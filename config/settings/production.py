e271a74a92cc846d62cd9d3aeb3d224f2ad1edc0
Kim Desrosiers <kimdesro@gmail.com>
2017-10-09 23:18:56 -0400

    Add sentry support for production environment.

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,3 +9,10 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['gobgift.kimond.com', ])
 
 ADMIN_URL = env('DJANGO_ADMIN_URL')
+
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
+
+SENTRY_DSN = env('DJANGO_SENTRY_DSN')
+SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
